a36c3ef76898a2c4c84ddd8d671af1db89fd28e7
pood0g <>
2023-07-01 20:23:55 +1000

    Refinements!

--- start_services.sh
+++ start_services.sh
@@ -4,17 +4,12 @@ cur_date () {
      date +'%Y/%m/%d %H:%M:%S' 
      }
 
-if [ ! -f /etc/stunnel/stunnel.pem ]
+if [ ! -f ./key.pem ] || [ ! -f ./cert.pem ]
 then
     echo "$(cur_date) First run, generating self signed TLS certificate."
-    openssl genrsa -out key.pem 4096
-    openssl req -new -x509 -key key.pem -out cert.pem -days 365 -subj "/CN=go_pia_wg_transmission"
+    openssl req -new -newkey rsa:4096 -nodes -x509 -keyout key.pem -out cert.pem -days 365 -subj "/CN=go_pia_wg_transmission"
     chown stunnel:stunnel key.pem cert.pem
 fi
-
-echo "$(cur_date) Starting stunnel"
-
-su stunnel -s /bin/ash -c "stunnel"
 
 echo "`cur_date` Starting go_pia"
 
